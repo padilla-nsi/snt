3060d30dcc9192e61bd2e4b4227b35d2e8f0cafb
Pskalou <pascal.padilla@gmail.com>
2021-09-08 15:25:04 +0200

    réseaux sociaux

---
+++ cours/reseaux_sociaux/_2de 8.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="SNT&#xa;2de 8" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1631095555071">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Le cours de &quot;SNT&quot;, c&apos;est quoi ?" FOLDED="true" POSITION="right" ID="ID_1994021212" CREATED="1631090017625" MODIFIED="1631095495594">
+<node TEXT="le programme de l&apos;année" FOLDED="true" ID="ID_1395845989" CREATED="1631095521252" MODIFIED="1631095525271">
+<node TEXT="Réseau sociaux" ID="ID_11783705" CREATED="1631090591308" MODIFIED="1631090609144"/>
+<node TEXT="Internet" ID="ID_1752652667" CREATED="1631090611852" MODIFIED="1631090617811"/>
+<node TEXT="Objets connectés" ID="ID_112376022" CREATED="1631090706098" MODIFIED="1631090710167"/>
+<node TEXT="Web" ID="ID_218535358" CREATED="1631090877819" MODIFIED="1631090904290"/>
+<node TEXT="photo numérique" ID="ID_1101193523" CREATED="1631090971004" MODIFIED="1631090980999"/>
+<node TEXT="géolocalisation" ID="ID_1753359726" CREATED="1631091017872" MODIFIED="1631091034422"/>
+<node TEXT="Données" ID="ID_578179447" CREATED="1631091087037" MODIFIED="1631091089012"/>
+</node>
+<node TEXT="carte mentale" FOLDED="true" ID="ID_1962758784" CREATED="1631095502480" MODIFIED="1631095506135">
+<node TEXT="technologie" ID="ID_429691066" CREATED="1631090079943" MODIFIED="1631090083136"/>
+<node TEXT="sciences" ID="ID_1106903103" CREATED="1631090121300" MODIFIED="1631090123572"/>
+<node TEXT="numérique" ID="ID_1497438321" CREATED="1631090129046" MODIFIED="1631090130852"/>
+<node TEXT="informatique" ID="ID_1803688524" CREATED="1631090141457" MODIFIED="1631090148110"/>
+<node TEXT="Physique chimie" ID="ID_1605775911" CREATED="1631090149081" MODIFIED="1631090166885"/>
+<node TEXT="tableur" ID="ID_1823314132" CREATED="1631090212445" MODIFIED="1631090214290"/>
+<node TEXT="cahier des charges" ID="ID_474052253" CREATED="1631090234818" MODIFIED="1631090237239"/>
+<node TEXT="capteur" ID="ID_1249741154" CREATED="1631090241839" MODIFIED="1631090247412"/>
+<node TEXT="logiciels" ID="ID_1627987665" CREATED="1631090254191" MODIFIED="1631090262783"/>
+<node TEXT="ordinateur" ID="ID_739600388" CREATED="1631090271714" MODIFIED="1631090273765"/>
+<node TEXT="wifi" ID="ID_1685857824" CREATED="1631090282286" MODIFIED="1631090290791"/>
+<node TEXT="internet" ID="ID_1476072002" CREATED="1631090299552" MODIFIED="1631090302370"/>
+<node TEXT="calculs" ID="ID_1254868460" CREATED="1631090304978" MODIFIED="1631090308806"/>
+<node TEXT="arduino" ID="ID_753280799" CREATED="1631090311322" MODIFIED="1631090313103"/>
+<node TEXT="google" ID="ID_1152483913" CREATED="1631090321648" MODIFIED="1631090322706"/>
+</node>
+</node>
+<node TEXT="Chap. 1 - Réseau social" POSITION="right" ID="ID_44248409" CREATED="1631095538089" MODIFIED="1631095546378">
+<node TEXT="Définition de &quot;réseau social&quot; ???" ID="ID_1924307839" CREATED="1631091175563" MODIFIED="1631091187056"/>
+</node>
+</node>
+</map>
