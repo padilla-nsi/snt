7654e3b5840e3e7a981a4a369c4ea28b41d5a1b1
Pskalou <pascal.padilla@gmail.com>
2021-09-11 10:20:38 +0200

    test export mm

--- cours/reseaux_sociaux/_Carte d'identité.mm
+++ cours/reseaux_sociaux/_Carte d'identité.mm
@@ -1,9 +1,9 @@
 <map version="freeplane 1.9.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Réseaux sociaux" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1630750015456">
+<node TEXT="Carte d&apos;identité" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1631347923846">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<font NAME="Roboto Condensed" BOLD="true"/>
-<hook NAME="MapStyle" background="#f9f9f8" zoom="0.826">
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.1">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
 
 <map_styles>
@@ -79,11 +79,24 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="Identité numérique" POSITION="left" ID="ID_243500434" CREATED="1630750019346" MODIFIED="1630750030290"/>
-<node TEXT="Réseaux existants" POSITION="left" ID="ID_1047629831" CREATED="1630750031831" MODIFIED="1630750039046"/>
-<node TEXT="Modèles économiques" POSITION="right" ID="ID_1686241487" CREATED="1630750041053" MODIFIED="1630750048167"/>
-<node TEXT="Graphes" POSITION="left" ID="ID_1727033079" CREATED="1630750049467" MODIFIED="1630750051270"/>
-<node TEXT="Petits mondes" POSITION="right" ID="ID_1210228283" CREATED="1630750052635" MODIFIED="1630750060877"/>
-<node TEXT="Cyberviolence" POSITION="right" ID="ID_115775030" CREATED="1630750062260" MODIFIED="1630750066170"/>
+<node TEXT="Date de mise en ligne" POSITION="left" ID="ID_1431819080" CREATED="1630869075526" MODIFIED="1631347976978"/>
+<node TEXT="Siège social" POSITION="left" ID="ID_1827149268" CREATED="1630869075527" MODIFIED="1631347971501"/>
+<node TEXT="Fondateur(s)" POSITION="left" ID="ID_1424488173" CREATED="1630869075527" MODIFIED="1631347978572"/>
+<node TEXT="Public visé" POSITION="left" ID="ID_1502056909" CREATED="1630869075532" MODIFIED="1631347979956"/>
+<node TEXT="Modèle économique" POSITION="left" ID="ID_762094744" CREATED="1630869075535" MODIFIED="1631347984164"/>
+<node TEXT="URL" POSITION="right" ID="ID_187223863" CREATED="1630869075526" MODIFIED="1631347936182">
+<node TEXT="adresse internet" ID="ID_1687379197" CREATED="1631347960816" MODIFIED="1631347968876"/>
+</node>
+<node TEXT="Nombre d’abonnés" POSITION="right" ID="ID_501733696" CREATED="1630869075528" MODIFIED="1631347950191">
+<node TEXT="voir la différence entre abonnés actifs et abonnés inactifs" ID="ID_1865905658" CREATED="1631347953230" MODIFIED="1631347957291"/>
+</node>
+<node TEXT="Finalité" POSITION="right" ID="ID_1064658658" CREATED="1630869075528" MODIFIED="1631347973337">
+<node TEXT="objectif, but du réseau social" ID="ID_1809281844" CREATED="1631348106062" MODIFIED="1631348113159"/>
+</node>
+<node TEXT="Style graphique" POSITION="right" ID="ID_305035630" CREATED="1631348052192" MODIFIED="1631348057739">
+<node TEXT="logo" ID="ID_1111688987" CREATED="1631348058898" MODIFIED="1631348060788"/>
+<node TEXT="couleurs" ID="ID_783720671" CREATED="1631348060993" MODIFIED="1631348062844"/>
+<node TEXT="..." ID="ID_1569359458" CREATED="1631348063266" MODIFIED="1631348065156"/>
+</node>
 </node>
 </map>
